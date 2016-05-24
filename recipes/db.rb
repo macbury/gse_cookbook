a227a558b6810659f5a5a5b58abf5c81a3517891
Arkadiusz Buras <macbury@gmail.com>
2016-05-24 14:50:52 +0200

    Switched db

--- recipes/db.rb
+++ recipes/db.rb
@@ -10,25 +10,30 @@ apt_repository 'pgdg' do
   deb_src true
 end
 
-postgresql postgresql_cluster_name do
-  cluster_version postgresql_version
-end
+include_recipe 'postgresql::default'
+include_recipe 'postgresql::server'
+include_recipe 'postgresql::ruby'
 
 databases = data_bag('gse_db')
+
+postgres_connection = {
+  host: '127.0.0.1',
+  username: 'postgres',
+  password: node['postgresql']['password']['postgres']
+}
 
 databases.each do |database_name|
   database = data_bag_item('gse_db', database_name)
 
-  postgresql_user database['user'] do
-    in_version postgresql_version
-    in_cluster postgresql_cluster_name
-    unencrypted_password database['password']
-  end
-
   postgresql_database database_name do
-    in_version postgresql_version
-    in_cluster postgresql_cluster_name
-    owner database['user']
+    connection(postgres_connection)
+    action :create
   end
 
+  postgresql_database_user database['user'] do
+    password database['password']
+    database_name database_name
+    privileges    [:all]
+    action        :grant
+  end
 end
--- recipes/default.rb
+++ recipes/default.rb
@@ -1,3 +1,5 @@
+include_recipe 'apt'
+
 include_recipe 'gse::db'
 include_recipe 'gse::ruby'
 include_recipe 'gse::application'
